c020c4c46b90c9f04a788ab5eabcb1cce0900639
Merit <meritxelladech@gmail.com>
2024-07-26 20:10:21 +0100

    Start the flask application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+""" This module starts the flask application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Welcoming message"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
