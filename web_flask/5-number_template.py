0ac75642d10ab8c7fda14fa3fce5261f4d72f838
Merit <meritxelladech@gmail.com>
2024-07-27 11:56:34 +0100

    Rendering an HTML page when the parameter is an integer

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" This module disaplays HTML page"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def welcome():
+    """The function displays Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """This function displays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<string:text>", strict_slashes=False)
+def c_message(text):
+    """This function displays the C message passed as URL parameter"""
+    res = text.replace("_", " ")
+    return f"C {res}"
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<string:text>", strict_slashes=False)
+def python(text="is cool"):
+    """The route response to python route"""
+    res = text.replace("_", " ")
+    return f"Python {res}"
+
+
+@app.route("/number/<int:num>", strict_slashes=False)
+def show_num(num):
+    """This function displays the number passed as URL parameter"""
+    return f"{num} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def display_page(n):
+    """This function displays an HTML page if num is an integer"""
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
